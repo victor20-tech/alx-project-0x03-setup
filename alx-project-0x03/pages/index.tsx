ec4689ece3a8053edfef051edfdd0c1436e867db
Victor Adomako <victoradomako10@gmail.com>
2025-11-13 16:09:54 +0000

    Update index.tsx

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
       {/* Welcome Message */}
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">  Welcome to Splash App! </h1>
+     <h1> Welcome to Splash App </h1>
       <p className="text-lg text-gray-600 mb-8">
         Your one-stop platform for everything AI you need. Start exploring by
         navigating to our features below.
